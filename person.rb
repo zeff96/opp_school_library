66fdd7ac31df17b795b9b485e70e76bcc8855717
zeff96 <zeffo1975@gmail.com>
2023-05-15 17:23:03 +0300

    Add person class with the following methods:   -public method def can_use_services   -private method def of_age

---
+++ person.rb
@@ -0,0 +1,26 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @id = 0
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    return true if @ge || @parent_permission
+    false
+  end
+
+  private
+
+  def of_age?
+    return true if @age >= 18
+    false
+  end
+end
+
+person = Person.new(18, "Adeka", false)
+person.of_age?
