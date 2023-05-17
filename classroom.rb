0345110c521bf8d90a222e2ad16b479ade42b729
zeff96 <zeffo1975@gmail.com>
2023-05-17 16:06:23 +0300

    Add class Classroom to the project

---
+++ classroom.rb
@@ -0,0 +1,15 @@
+require_relative 'student'
+
+class Classroom
+  attr_accessor :label, :student
+
+  def initialize(label)
+    @label = label
+    @student = []
+  end
+
+  def add_student(student)
+    student.classrom = self
+    @student << student
+  end
+end
