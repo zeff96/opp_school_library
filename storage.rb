d7082be0c8c4abe048c379556fd05c182adf67c8
zeff96 <zeffo1975@gmail.com>
2023-10-24 19:15:27 +0300

    Add method for loading data from json

--- storage.rb
+++ storage.rb
@@ -8,4 +8,12 @@ class DataManager
   def save_to_json(data)
     File.write(@filename, JSON.generate(data))
   end
+
+  def load_from_json
+    return [] unless File.exist?(@filename)
+
+    File.open(@filename, 'r') do |file|
+      JSON.parse(file.read, symbolize_names: true)
+    end
+  end
 end
