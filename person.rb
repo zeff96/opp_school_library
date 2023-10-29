80003c421ed3280e6ccbfaf4f736175e685630f8
zeff96 <zeffo1975@gmail.com>
2023-10-29 19:20:20 +0300

    Add person parent permission validation test

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ require_relative 'rental'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age, :rental
+  attr_accessor :name, :age, :parent_permission, :rental
 
   def initialize(age, name: 'Unknown', parent_permission: true)
     super()
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../person'
 
 describe Person do
-  let(:person) { Person.new(22, name: 'Test', parent_permission: 'Yes') }
+  let(:person) { Person.new(22, name: 'Test', parent_permission: false) }
   context 'validates presence of person class attributes' do
     it 'returns person age' do
       expect(person.age).to eq(22)
@@ -9,6 +9,10 @@ describe Person do
 
     it 'returns persons name' do
       expect(person.name).to eq 'Test'
+    end
+
+    it 'returns persons parent_permission' do
+      expect(person.parent_permission).to be_falsy
     end
   end
 end
