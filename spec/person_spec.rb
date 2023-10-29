912be9093e70b7c8c0c952d6a24d9cf253cc22b8
zeff96 <zeffo1975@gmail.com>
2023-10-29 19:06:25 +0300

    Add person age validations test

---
+++ spec/person_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../person'
+
+describe Person do
+  let(:person) { Person.new(22, name: 'Test', parent_permission: 'Yes') }
+  context 'validates presence of person class attributes' do
+    it 'returns person age' do
+      expect(person.age).to eq(22)
+    end
+  end
+end
