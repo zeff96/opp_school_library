bdd9e851f819321e4c7c348c78d942a8c9c66f91
zeff96 <zeffo1975@gmail.com>
2023-10-31 12:48:03 +0300

    Add rental date test validation

---
+++ spec/rental_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../rental'
+require_relative '../book'
+require_relative '../person'
+
+describe Rental do
+  let(:book) { Book.new('Mathematics', 'Longhorn') }
+  let(:person) { Person.new(22, name: 'Test', parent_permission: true) }
+  let(:rental) { Rental.new('2023-01-01', book, person) }
+
+  context 'Validates rental attributes' do
+    it 'returns rental date' do
+      expect(rental.date).to eq '2023-01-01'
+    end
+  end
+end
