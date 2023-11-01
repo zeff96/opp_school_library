a0aa8b931e4fcfd0daf92c55d2e014b3db80d49d
zeff96 <zeffo1975@gmail.com>
2023-11-01 08:19:45 +0300

    Update aposthrophes escape

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,15 +6,15 @@ describe Person do
   let(:person) { Person.new(22, name: 'Test', parent_permission: false) }
 
   context 'validates presence of person class attributes' do
-    it 'returns person age' do
+    it 'returns person\'s age' do
       expect(person.age).to eq(22)
     end
 
-    it 'returns persons name' do
+    it 'returns person\'s name' do
       expect(person.name).to eq 'Test'
     end
 
-    it 'returns persons parent_permission' do
+    it 'returns person\'s parent_permission' do
       expect(person.parent_permission).to be_falsy
     end
 
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -4,15 +4,15 @@ describe Teacher do
   let(:teacher) { Teacher.new(50, 'Engineer', name: 'Mark') }
 
   context 'Validate teacher attributes' do
-    it 'returns \teacher\'s age' do
+    it 'returns teacher\'s age' do
       expect(teacher.age).to eq 50
     end
 
-    it 'returns \teacher\'s speciliazation' do
+    it 'returns teacher\'s speciliazation' do
       expect(teacher.specialization).to eq 'Engineer'
     end
 
-    it 'returns \teacher\'s name' do
+    it 'returns teacher\'s name' do
       expect(teacher.name).to eq 'Mark'
     end
   end
