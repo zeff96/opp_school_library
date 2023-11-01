315e2cb22db1be741b00599899c91e8f115d50cb
zeff96 <zeffo1975@gmail.com>
2023-11-01 07:48:47 +0300

    Add decorator file refactor

--- decorator.rb
+++ decorator.rb
@@ -12,16 +12,3 @@ class Decorator < Nameable
     @nameable.correct_name
   end
 end
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    name = @nameable.correct_name
-    name.length > 10 ? name[0, 10].strip : name.strip
-  end
-end
